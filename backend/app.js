9bec764f76537c5e91c69167898ce1843e28cff6
Erick Agrazal <erick.agrazal@gmail.com>
2021-06-07 19:36:44 -0500

    Added frontend integration with backend.

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const cors = require('cors');
+const app = express();
+const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 const Data = [
     {
@@ -34,7 +37,10 @@ const Users = {
         res.json(Data[req.params.id]);
     },
     createUser: (req, res) => {
-        Data.push(req.body);
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){ return res.json(errors);}
+        const { name, age, status } = req.body;
+        Data.push({ name, age, status });
         res.json({
             model: 'Users',
             count: Data.length,
@@ -43,9 +49,20 @@ const Users = {
     }
 }
 
+const UsersValidations = {
+    createUser: [
+        body('name', 'El nombre es incorrecto.').exists({checkNull: true, checkFalsy: true}),
+        body('age', 'La edad es incorrecta.').isNumeric(),
+        body('status', 'El estatus es incorrecto.').exists({checkNull: true, checkFalsy: true}).isIn(['active', 'inactive']),
+    ]
+}
+
 app.get('/api/v1/users/', Users.getUsers);
 app.get('/api/v1/users/:id', Users.getUser);
-app.post('/api/v1/users/', Users.createUser);
+app.post('/api/v1/users/', UsersValidations.createUser, Users.createUser);
+// app.put('/api/v1/users/:id', Users.getUser);
+// app.patch('/api/v1/users/:id', Users.getUser);
+// app.delete('/api/v1/users/:id', Users.getUser);
 
 app.listen(port, () => {
     console.log(`Ejemplo escuchando en: http://localhost:${port}`)
